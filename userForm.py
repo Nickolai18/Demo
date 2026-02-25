346d80e2e5477be5565e7f5a3845e86d47c4e182
admin <nick.rotari.42@gmail.com>
2026-02-25 15:43:15 +0300

    test

--- userForm.py
+++ userForm.py
@@ -3,16 +3,24 @@ import customtkinter as ctk
 appTest = ctk.CTk()
 appTest.geometry("400x400")
 appTest.title("First Example")
+for i in range(3):
+    appTest.grid_columnconfigure(i, weight=1)
+    appTest.grid_rowconfigure(i, weight=1)
+# labelUsername = ctk.CTkLabel(appTest, text="Username")
+# labelUsername.grid(row=0, column=0)
+#
+# labelPassword = ctk.CTkLabel(appTest, text="Password")
+# labelPassword.grid(row=1, column=0)
 
-labelUsername = ctk.CTkLabel(appTest, text="Username")
-labelUsername.grid(row=0, column=0)
+entryName = ctk.CTkEntry(appTest, width=120, height=20, placeholder_text="Name")
+entryName.pack(padx=5, pady=75)
 
-labelPassword = ctk.CTkLabel(appTest, text="Password")
-labelPassword.grid(row=1, column=0)
+entryPassword = ctk.CTkEntry(appTest, width=120, height=20, placeholder_text="Password")
+entryPassword.pack(padx=10, pady=10)
 
 # button = ctk.CTkButton(appTest, text="firstButton", width="50", height="39", fg_color="black")
-# button = ctk.CTkButton(appTest, text="firstButton")
-# button.grid(row=3, column=0, padx=20, pady=10)
+button = ctk.CTkButton(appTest, text="firstButton")
+button.pack(padx=10, pady=10)
 
 appTest.mainloop()
 # ctk.set_default_color_theme("white")
