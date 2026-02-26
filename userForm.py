6006cd52fb02fa277c7f392c19acc0bb7e1ff3aa
admin <nick.rotari.42@gmail.com>
2026-02-26 12:44:48 +0300

    Complete Form

--- userForm.py
+++ userForm.py
@@ -3,23 +3,28 @@ import customtkinter as ctk
 appTest = ctk.CTk()
 appTest.geometry("400x400")
 appTest.title("First Example")
-for i in range(3):
-    appTest.grid_columnconfigure(i, weight=1)
-    appTest.grid_rowconfigure(i, weight=1)
+widthEntry = 250
+heightEntry = 25
 # labelUsername = ctk.CTkLabel(appTest, text="Username")
 # labelUsername.grid(row=0, column=0)
 #
 # labelPassword = ctk.CTkLabel(appTest, text="Password")
 # labelPassword.grid(row=1, column=0)
 
-entryName = ctk.CTkEntry(appTest, width=120, height=20, placeholder_text="Name")
-entryName.pack(padx=5, pady=75)
+frameForm = ctk.CTkFrame(appTest, width=300)
+frameForm.pack(anchor="center", expand=1, padx=5, pady=5)
 
-entryPassword = ctk.CTkEntry(appTest, width=120, height=20, placeholder_text="Password")
-entryPassword.pack(padx=10, pady=10)
+formLabel = ctk.CTkLabel(frameForm, text="Введите имя и пароль", font=('Arial', 25))
+formLabel.pack(anchor="n")
+
+entryName = ctk.CTkEntry(frameForm, width=widthEntry, height=heightEntry, placeholder_text="Name")
+entryName.pack(padx=10, pady=10)
+
+entryPassword = ctk.CTkEntry(frameForm, width=widthEntry, height=heightEntry, placeholder_text="Password")
+entryPassword.pack()
 
 # button = ctk.CTkButton(appTest, text="firstButton", width="50", height="39", fg_color="black")
-button = ctk.CTkButton(appTest, text="firstButton")
+button = ctk.CTkButton(frameForm, text="firstButton")
 button.pack(padx=10, pady=10)
 
 appTest.mainloop()
